c1f71a14740758b4512b26b0550115453162646a
salamaxx97 <mohamedsalama20072@gmail.com>
2026-04-22 21:25:51 +0300

    Solving ispn

---
+++ isbn-verifier/isbn_verifier.py
@@ -0,0 +1,24 @@
+"""is it like isbn-10 """ 
+def is_valid(isbn):
+    """is it like isbn-10 """ 
+    # Remove dashes to make indexing easier
+    clean_isbn = isbn.replace("-", "")
+    
+    if len(clean_isbn) != 10:
+        return False
+        
+    total = 0
+    for i in range(10):
+        char = clean_isbn[i]
+        
+        if char.isdigit():
+            value = int(char)
+        elif char in "xX" and i == 9: # 'X' is only valid at the 10th position
+            value = 10
+        else:
+            # If we find a letter anywhere else, or a non-X at the end
+            return False
+            
+        total += (10 - i) * value
+        
+    return total % 11 == 0
