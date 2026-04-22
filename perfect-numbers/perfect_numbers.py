aca3fa059c735ed13722039dde8f81532df55a02
salamaxx97 <mohamedsalama20072@gmail.com>
2026-04-22 22:43:48 +0300

    Solving perfect_numbers after review

--- perfect-numbers/perfect_numbers.py
+++ perfect-numbers/perfect_numbers.py
@@ -7,7 +7,7 @@ def classify(number):
     """
     if number < 1 :
         raise ValueError("Classification is only possible for positive integers.")
-
+        return None
     total = 0
     for count in range(1,number):
         if number % count == 0 :
