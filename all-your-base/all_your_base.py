7f2a1c30e2d75033209dd667d965091d23303af1
salamaxx97 <mohamedsalama20072@gmail.com>
2026-04-23 00:30:51 +0300

    Solving all_bases convert to base10 after review

--- all-your-base/all_your_base.py
+++ all-your-base/all_your_base.py
@@ -5,20 +5,20 @@ def rebase(input_base, digits, output_base):
     if input_base < 2 :
         raise ValueError("input base must be >= 2")
     if not all(0 <= digit < input_base for digit in digits):
-            raise ValueError("all digits must satisfy 0 <= d < input base")
+        raise ValueError("all digits must satisfy 0 <= d < input base")
     if output_base < 2 :
         raise ValueError("output base must be >= 2")
 
     base_10 = 0
     output = [] 
     for index,digit in enumerate(digits[::-1]):
-        base_10+=digit*input_base**index
+        base_10+=digit *input_base ** index
     
     if base_10 == 0 : return [0]
 
     while base_10 != 0 :
         output.append(base_10 % output_base)
-        base_10 =int(base_10 / output_base)
+        base_10 =base_10 // output_base
 
     return output[::-1]
 
