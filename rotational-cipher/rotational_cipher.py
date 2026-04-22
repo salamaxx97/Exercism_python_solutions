5ba9eb6905f4b9523cbd6f7ed47e670a6ffe3174
salamaxx97 <mohamedsalama20072@gmail.com>
2026-04-22 21:57:29 +0300

    Solving rotational cipher

---
+++ rotational-cipher/rotational_cipher.py
@@ -0,0 +1,18 @@
+import re
+from string import ascii_lowercase,ascii_uppercase
+""" crypto using rotcipher""" 
+def rotate(text, key):
+    """ crypto usin grotcipher""" 
+    key = key % 26
+    lower_alpha = ascii_lowercase
+    upper_alpha = ascii_uppercase
+    alphapet=upper_alpha+lower_alpha
+
+    shifted_lower=lower_alpha[key:]+lower_alpha[:key]
+    shifted_upper=upper_alpha[key:]+upper_alpha[:key]
+    shifted = shifted_upper+shifted_lower
+    table = str.maketrans(alphapet,shifted)
+
+    return text.translate(table)
+
+    
