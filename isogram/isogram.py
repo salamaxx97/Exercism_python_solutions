72128eca5043417089b7f8550277e1b8f65db3ef
salamaxx97 <mohamedsalama20072@gmail.com>
2026-04-22 20:46:06 +0300

    Solving isogram

--- isogram/isogram.py
+++ isogram/isogram.py
@@ -1,2 +1,4 @@
-def is_isogram(string):
-    pass
+""" is it isogram ??? """
+def is_isogram(word):
+    """ is it isogram ??? """
+    return not any(letter.isalpha() and word.lower().count(letter.lower()) > 1 for letter in word ) 
